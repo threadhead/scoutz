928d9616a306faec93121533ac69abd4d38c2469
Karl Smith <threadhead@gmail.com>
2012-05-15 15:16:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoutz::Application.config.secret_token = '378d489525bc3170b63a980643195620437d07ef5820d25e63182d9bbae2edd26924212850b3352ff47858d8554f0d81763166ab716aab029f3ee61d3dc39c7f'

526b98343b40e0b2a95fa3e93af7848e06f182a1
Karl Smith <threadhead@gmail.com>
2012-05-18 23:36:52 -0700

    use carmen-rails helpers, fix org view specs

--- spec/models/sub_unit_spec.rb
+++ spec/models/sub_unit_spec.rb
@@ -4,7 +4,7 @@ describe SubUnit do
   it { should belong_to(:organization) }
   it { should validate_presence_of(:name) }
 
-  it 'validates uniqueness of name', :focus do
+  it 'validates uniqueness of name' do
   	FactoryGirl.create(:sub_unit)
   	should validate_uniqueness_of(:name).case_insensitive.scoped_to(:organization_id)
   end

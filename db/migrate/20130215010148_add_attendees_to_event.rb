db4f9815b19afc7b04a2fabd2527ddd1d00cba5d
Karl Smith <threadhead@gmail.com>
2013-02-14 19:37:59 -0700

    add attending count to event show, render partial as collection

--- db/migrate/20130215010148_add_attendees_to_event.rb
+++ db/migrate/20130215010148_add_attendees_to_event.rb
@@ -1,5 +1,5 @@
 class AddAttendeesToEvent < ActiveRecord::Migration
   def change
-    add_column :events, :attendee_count, :integer
+    add_column :events, :attendee_count, :integer, default: 0
   end
 end

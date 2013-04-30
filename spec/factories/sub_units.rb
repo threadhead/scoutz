b023f85b89514421b45cbef3d028895b77b7edd8
Karl Smith <threadhead@gmail.com>
2013-04-29 18:11:55 -0700

    add cub/boy sub unit types

--- spec/factories/sub_units.rb
+++ spec/factories/sub_units.rb
@@ -1,10 +1,22 @@
 FactoryGirl.define do
-  
-  sequence :sub_unit_name do |n|
-  	"Den #{n+1}"
+
+  sequence :sub_unit_name_cubs do |n|
+    "Den #{n+1}"
   end
-  
+
+  sequence :sub_unit_name_boys do |n|
+  	"Patrol #{n+1}"
+  end
+
   factory :sub_unit do
-		name			{ FactoryGirl.generate(:sub_unit_name) }
+		name			{ FactoryGirl.generate(:sub_unit_name_cubs) }
+
+    factory :cub_scout_sub_unit do
+      name      { FactoryGirl.generate(:sub_unit_name_cubs) }
+    end
+
+    factory :boy_scout_sub_unit do
+      name      { FactoryGirl.generate(:sub_unit_name_boys) }
+    end
 	end
 end

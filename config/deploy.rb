a4fd09309aa41a2595ccb9dd8b636a2147bb9bcc
Karl Smith <threadhead@gmail.com>
2013-08-27 16:35:30 -0700

    add shared_children for uploads

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,9 @@ set :use_sudo, false
 
 set :asset_env, ""
 
+set :shared_children, shared_children + %w{public/uploads}
+
+
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 

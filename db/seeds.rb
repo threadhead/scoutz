438f186ba9d5acd6930d924438f6e42525514b3b
Karl Smith <threadhead@gmail.com>
2014-05-05 23:34:47 -0700

    fix log typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,7 +38,7 @@ user2.scouts << scout3
 
 
 user3 = Adult.create(email: 'stoya.robert@orbital.com', password: 'pack1134', first_name: 'Rob', last_name: 'Stoya', time_zone: 'Arizona', confirmed_at: 1.day.ago)
-log_item(user2, 2)
+log_item(user3, 2)
 
 scout4 = Scout.create(first_name: 'Robbie', last_name: 'Stoya', time_zone: 'Arizona', birth: '2002-02-02'.to_date, rank: 'Webelos I')
 log_item(scout4, 4)

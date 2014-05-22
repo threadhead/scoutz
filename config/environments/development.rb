c01cd349eba03cf38aaceba9ea03e10d7bfa0afa
Karl Smith <threadhead@gmail.com>
2014-05-22 15:05:42 -0700

    dont load if VCR not present

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,4 @@
 Scoutz::Application.configure do
-  WebMock.disable!
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on

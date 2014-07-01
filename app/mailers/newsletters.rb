96404b6d96e8c470f5204549c1d8039639bb1e8d
Karl Smith <threadhead@gmail.com>
2014-06-30 17:24:10 -0700

    work on monthly newsletter

--- app/mailers/newsletters.rb
+++ app/mailers/newsletters.rb
@@ -3,17 +3,21 @@ class Newsletters < ActionMailer::Base
 
   def weekly(unit_id)
     set_unit_recipients(unit_id)
-    @events = @unit.events.from_today.where('start_at <= ?', 9.days.from_now.end_of_day).by_start
+    @events = @unit.events.newsletter_week.by_start
+    @subject = "Upcoming Events for the week of #{Time.zone.now.next_week.to_s(:draft_date)}"
 
     mail to: @recipients,
-         subject: "#{@unit.email_name} Upcoming Events for the week of "
+         subject: "#{@unit.email_name} #{@subject}"
   end
 
   def monthly(unit_id)
     set_unit_recipients(unit_id)
+    @events = @unit.events.in_next_month.by_start
+    @subject = "Upcoming Events for the month of #{Time.zone.now.next_month.strftime('%B %Y')}"
 
     mail to: @recipients,
-         subject: "#{@unit.email_name} Upcoming Events for the week of "
+         subject: "#{@unit.email_name} #{@subject}",
+         template_name: 'weekly'
   end
 
 

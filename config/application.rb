10b3f4d0eae9c9c0ce1d4dc0115d51e1e30d173a
Karl Smith <threadhead@gmail.com>
2014-06-30 17:23:57 -0700

    change weekday start to sunday

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Scoutz
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.beginning_of_week = :sunday
 
     config.i18n.enforce_available_locales = false
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.

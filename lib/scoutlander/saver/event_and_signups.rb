8ae78eb4761b2f8ae1d5a06667193b65b61bdd70
Karl Smith <threadhead@gmail.com>
2014-07-19 14:52:00 -0700

    improve event signup creation, better logging

--- lib/scoutlander/saver/event_and_signups.rb
+++ lib/scoutlander/saver/event_and_signups.rb
@@ -44,6 +44,12 @@ module Scoutlander
 
 
       def create_or_update_event_signups
+        if @datum.event_signups.size == 0
+          @logger.info "CREATE_EVENT_SIGNUP: #{@datum.name}, no event signups to create"
+          return
+        else
+          @logger.info "CREATE_EVENT_SIGNUP: #{@datum.name}, event_signups: #{@datum.event_signups.size}"
+        end
         # we wrap all the creating and updates of signups for two reasons:
         #  - if there is an error creating the signups, we don't want existing signups deleted
         #  - there is a possibiliy of a "bad" signup that would cause deletion of all existing signups, so
@@ -51,27 +57,29 @@ module Scoutlander
 
         ActiveRecord::Base.transaction do
           # find all the existing signups, to be removed after creation of new signups
-          existing_signups = @event.event_signups.all.load
+          existing_signups = @event.event_signups.pluck(:id)
 
           at_least_one_signup = false
           @datum.event_signups.each do |signup|
             if signup.valid?
-              scout = Scout.where(sl_profile: signup.sl_profile).first
-              if scout
-                @event.event_signups.create(signup.to_params.merge({scout_id: scout.id}))
+              if scout = Scout.where(sl_profile: signup.sl_profile).first
                 @logger.info "CREATE_EVENT_SIGNUP: #{@datum.name}, scout profile: #{signup.sl_profile}, s:#{signup.scouts_attending}, sib:#{signup.siblings_attending}, a:#{signup.adults_attending}"
 
+                # we need to turn off validataions because we ARE going to save duplicates, but later
+                #   remove the existing records below. Thus, we will be left with only the new signups
+                event_signup = @event.event_signups.build(signup.to_params.merge({scout_id: scout.id}))
+                event_signup.save(validate: false)
+                at_least_one_signup = true
               else
-                @logger.info "CANCELLED EVENT_SIGNUP: scout not found, #{@datum.name}, profile: #{@datum.sl_profile}"
+                @logger.info "CANCELLED EVENT_SIGNUP: scout not found, scout profile: #{@datum.sl_profile}"
               end
 
-              at_least_one_signup = true
             end
           end
 
           if at_least_one_signup
-            @logger.info "REMOVE_EXISTING_EVENT_SIGNUPS: #{@datum.name}, count: #{existing_signups.size}"
-            existing_signups.destroy_all
+            @logger.info "REMOVE_EXISTING_EVENT_SIGNUPS: #{@datum.name}, count: #{existing_signups.size}, ids: #{existing_signups}"
+            EventSignup.destroy_all(id: existing_signups)
           else
             @logger.info "ROLLBACK - EVENT_SIGNUPS: #{@datum.name}, there was not at least one new signup imported"
            raise ActiveRecord::Rollback

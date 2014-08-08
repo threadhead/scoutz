3e8455efd2b4337564a0c8d01f9638b2db6c9c02
Karl Smith <threadhead@gmail.com>
2014-08-08 04:50:52 -0700

    tried moving ckeditor returned js into view, no success

--- app/controllers/ckeditor/pictures_controller.rb
+++ app/controllers/ckeditor/pictures_controller.rb
@@ -12,21 +12,22 @@ class Ckeditor::PicturesController < Ckeditor::BaseController
   def create
     h = Hash.new
     h[:picture] = {data: params[:upload]}
-    # logger.info h.inspect
     @picture = Ckeditor::Picture.new(h[:picture])
     @picture.unit_id = params[:unit_id]
     @picture.user_id = current_user.id
 
     authorize @picture
     @picture.save
-    # logger.info @picture.inspect
     # respond_with_asset(@picture)
     body = %Q"<script type='text/javascript'>
           window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '#{@picture.data_url}');
         </script>"
 
-    # logger.info body
+    # # logger.info body
     render text: body
+    # render :create, format: :txt
+    # logger.info render_to_string('create.txt.haml')
+    # render partial: 'create.txt.haml'
   end
 
 

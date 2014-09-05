4bbcc7f2bd3c2dcd8013660c8187f2d78a8a060d
Karl Smith <threadhead@gmail.com>
2014-09-04 17:04:10 -0700

    update gems

--- spec/models/merit_badge_spec.rb
+++ spec/models/merit_badge_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe MeritBadge do
-  it { is_expected.to have_and_belong_to_many(:users) }
+  it { is_expected.to have_many(:users).through(:counselors) }
+  it { is_expected.to have_many(:counselors) }
 
 
   describe 'validators' do

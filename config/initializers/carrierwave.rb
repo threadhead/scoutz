1b21783d0a63dea5c00ad434ebe37e79b6495ba9
Karl Smith <threadhead@gmail.com>
2014-10-07 16:52:09 -0700

    remove region, causes 302 errors

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ if Rails.env.production? || Rails.env.staging? || Rails.env.development?
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+      # :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
       # :host                   => 's3.example.com',             # optional, defaults to nil
       # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }

076bb493f45f44441aa3ffe4fe6b58ee2110ad08
Karl Smith <threadhead@gmail.com>
2014-10-31 12:11:26 -0700

    turn off ssl for after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,7 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
       set_user_units
       set_unit
-      stored_location_for(resource) || unit_events_url(@unit)
+      stored_location_for(resource) || unit_events_url(@unit, protocol: 'http')
     end
 
 end

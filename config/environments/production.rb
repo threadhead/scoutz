ad58a15dbb77fe87e020c76d3e32dfe755daf3e1
Karl Smith <threadhead@gmail.com>
2014-10-31 11:54:10 -0700

    try using ssl only for signins and resets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Scoutz::Application.configure do
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -106,4 +106,10 @@ Scoutz::Application.configure do
       :sender_address => %{"scoutt.in notifier" <notifier@scoutt.in>},
       :exception_recipients => %w{threadhead@gmail.com}
     }
+
+
+  config.to_prepare { SessionsController.force_ssl }
+  config.to_prepare { RegistrationsController.force_ssl }
+  config.to_prepare { PasswordsController.force_ssl }
+
 end

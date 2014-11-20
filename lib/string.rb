0f8ee38ecfa1e4590900f60fd8b76d7f1e8cee20
Karl Smith <threadhead@gmail.com>
2014-11-19 18:36:45 -0700

    add possessive to string class

--- lib/string.rb
+++ lib/string.rb
@@ -3,3 +3,16 @@
 #     self.blank? ? '' : after ? "#{self}, " : ", #{self}"
 #   end
 # end
+
+
+# from: https://github.com/bclubb/possessive/blob/master/lib/possessive.rb
+class String
+  def possessive
+    return self if self.empty?
+    self + ('s' == self[-1,1] ? Possessive::APOSTROPHE_CHAR : Possessive::APOSTROPHE_CHAR+"s")
+  end
+end
+
+module Possessive
+  APOSTROPHE_CHAR = '’'
+end

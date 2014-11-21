df0a56176a6d93a596579cb01a4af6e3b4835251
Karl Smith <threadhead@gmail.com>
2014-11-21 15:00:23 -0700

    add possessive to string class

--- lib/string.rb
+++ lib/string.rb
@@ -1,11 +1,4 @@
-# class String
-#   def commaize(after=true)
-#     self.blank? ? '' : after ? "#{self}, " : ", #{self}"
-#   end
-# end
-
-
-# from: https://github.com/bclubb/possessive/blob/master/lib/possessive.rb
+# # from: https://github.com/bclubb/possessive/blob/master/lib/possessive.rb
 class String
   def possessive
     return self if self.empty?
@@ -16,3 +9,9 @@ end
 module Possessive
   APOSTROPHE_CHAR = '’'
 end
+
+# class String
+#   def commaize(after=true)
+#     self.blank? ? '' : after ? "#{self}, " : ", #{self}"
+#   end
+# end

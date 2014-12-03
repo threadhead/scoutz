bf4b567930ef5918e2e62fec2a64eb2343f21188
Karl Smith <threadhead@gmail.com>
2014-12-03 13:55:48 -0700

    try reducing log level to avoid logging of /ping

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,6 +1,10 @@
 class StatusController < ApplicationController
   def ping
+    log_level = Rails.logger.loglevel
+    Rails.logger.level = Logger::WARN
     render text: 'ok', status: :ok, content_type: Mime::TEXT
+    Rails.logger.level = log_level
+
   end
 
   def health

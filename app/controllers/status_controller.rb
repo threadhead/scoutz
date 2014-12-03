61236045a29aca827ba73f71801f242657696378
Karl Smith <threadhead@gmail.com>
2014-12-03 14:13:27 -0700

    only eliminated on log line, not worth it

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,10 +1,9 @@
 class StatusController < ApplicationController
   def ping
-    log_level = Rails.logger.level
-    Rails.logger.level = Logger::WARN
+    # log_level = Rails.logger.level
+    # Rails.logger.level = Logger::WARN
     render text: 'ok', status: :ok, content_type: Mime::TEXT
-    Rails.logger.level = log_level
-
+    # Rails.logger.level = log_level
   end
 
   def health

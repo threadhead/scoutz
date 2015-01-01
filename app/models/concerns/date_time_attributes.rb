4e89190c062c1f7cd4d4971624aa740875eca4b3
Karl Smith <karl@Karls-iMac.local>
2014-12-31 18:17:28 -0700

    introducing date_time_attributes concern to allow the splitting of datetimes for separate input

---
+++ app/models/concerns/date_time_attributes.rb
@@ -0,0 +1,74 @@
+module DateTimeAttributes
+  extend ActiveSupport::Concern
+
+  included do
+    # before_validation :attributes_to_datetime
+  end
+
+  module ClassMethods
+
+    # def date_time_attributes
+    #   @date_time_attributes || []
+    # end
+
+
+    def date_time_attribute(*attributes)
+      # @date_time_attributes = attributes
+      # opts = attributes.extract_options!
+
+      attributes.each do |attribute|
+        attribute = attribute.to_sym
+
+        define_method("#{attribute}_date") do
+          self.send(attribute).strftime("%b %d, %Y") if self.send(attribute).present?
+        end
+
+        define_method("#{attribute}_time") do
+          self.send(attribute).strftime("%I:%M%P") if self.send(attribute).present?
+        end
+
+        define_method("#{attribute}_date=") do |date|
+          begin
+            self.instance_variable_set("@#{attribute}_date", Date.parse(date).strftime("%Y-%m-%d"))
+          rescue ArgumentError
+            self.instance_variable_set("@#{attribute}_date", nil)
+          end
+          self.send("set_#{attribute}_datetime")
+        end
+
+        define_method("#{attribute}_time=") do |time|
+          begin
+            self.instance_variable_set("@#{attribute}_time", Time.parse(time).strftime("%H:%M:%S"))
+          rescue ArgumentError
+            self.instance_variable_set("@#{attribute}_time", nil)
+          end
+          self.send("set_#{attribute}_datetime")
+        end
+
+        define_method("set_#{attribute}_datetime") do
+          date = self.instance_variable_get("@#{attribute}_date")
+          time = self.instance_variable_get("@#{attribute}_time")
+          begin
+            self.send("#{attribute}=", Time.zone.parse("#{date} #{time}"))
+          rescue ArgumentError
+            self.send("#{attribute}=", nil)
+          end
+        end
+
+      end
+
+    end
+  end
+
+  # def attributes_to_datetime
+  #   self.class.date_time_attributes.each do |attribute|
+  #     date = self.instance_variable_get("@#{attribute}_date")
+  #     time = self.instance_variable_get("@#{attribute}_time")
+  #     begin
+  #       self.send("#{attribute}=", DateTime.parse("#{date} #{time}"))
+  #     rescue ArgumentError
+  #       self.send("#{attribute}=", nil)
+  #     end
+  #   end
+  # end
+end
---
+++ spec/models/concerns/date_time_attributes_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe 'DateTimeAttributes Concern' do
+  before(:all) do
+    ActiveRecord::Migration.verbose = false
+    ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+    ActiveRecord::Schema.define(:version => 1) do
+      create_table :attr_items do |t|
+        t.datetime :start_at
+        t.datetime :end_at
+      end
+    end
+
+    class AttrItem < ActiveRecord::Base
+      include DateTimeAttributes
+      date_time_attribute :start_at
+    end
+  end
+
+  after(:all) do
+    ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations['test'])
+  end
+
+  let(:attr_item) { AttrItem.new(start_at: '2014-02-02 08:08:08') }
+
+
+  describe 'creates ATTR_date/= and ATTR_time/= methods' do
+    specify { expect(AttrItem.method_defined?("start_at_date")).to eq(true) }
+    specify { expect(AttrItem.method_defined?("start_at_date=")).to eq(true) }
+    specify { expect(AttrItem.method_defined?("start_at_time")).to eq(true) }
+    specify { expect(AttrItem.method_defined?("start_at_time=")).to eq(true) }
+  end
+
+  describe 'ATTR_date and ATTR_time will be set to appropriate values' do
+    specify { expect(attr_item.start_at_date).to eq('Feb 02, 2014') }
+    specify { expect(attr_item.start_at_time).to eq('08:08am') }
+  end
+
+  it 'setting ATTR_date and ATTR_time updates ATTR' do
+    attr_item.start_at_date = "Aug 22, 1999"
+    attr_item.start_at_time = "09:09pm"
+    expect(attr_item.start_at).to be_within(1).of(Time.zone.parse '1999-08-22 21:09:00')
+
+    attr_item.start_at_time = "03:03pm"
+    attr_item.start_at_date = "Apr 12, 1991"
+    expect(attr_item.start_at).to be_within(1).of(Time.zone.parse '1991-04-12 15:03:00')
+  end
+
+  it 'sets ATTR to nil if bad dates or times' do
+    attr_item.start_at_date = "asdf"
+    expect(attr_item.start_at).to eq(nil)
+
+    attr_item.start_at_time = "asdfasdf"
+    expect(attr_item.start_at).to eq(nil)
+  end
+
+  it 'does not affect non-specified attributes' do
+    expect(AttrItem.method_defined?("end_at_date")).to eq(false)
+    expect(AttrItem.method_defined?("end_at_date=")).to eq(false)
+    expect(AttrItem.method_defined?("end_at_time")).to eq(false)
+    expect(AttrItem.method_defined?("end_at_time=")).to eq(false)
+  end
+end

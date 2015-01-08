6498bf73734b6b18901456e8153fd8c4c04cef19
Karl Smith <karl@Karls-iMac.local>
2015-01-08 13:44:22 -0700

    use metal for controller (small speedup)

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,12 +1,18 @@
-class StatusController < ApplicationController
+class StatusController < ActionController::Metal
   def ping
     # log_level = Rails.logger.level
     # Rails.logger.level = Logger::WARN
-    render text: 'ok', status: :ok, content_type: Mime::TEXT
+    # render text: 'ok', status: :ok, content_type: Mime::TEXT
     # Rails.logger.level = log_level
+    self.status = :ok
+    self.content_type = Mime::TEXT
+    self.response_body = 'ok'
   end
 
   def health
-    render text: 'ok', status: :ok, content_type: Mime::TEXT
+    # render text: 'ok', status: :ok, content_type: Mime::TEXT
+    self.status = :ok
+    self.content_type = Mime::TEXT
+    self.response_body = 'ok'
   end
 end

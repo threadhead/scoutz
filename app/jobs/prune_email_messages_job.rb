3c81b618aec89baf28e430f1543f12ace2bf3b60
Karl Smith <karl@Karls-iMac.local>
2015-01-29 23:50:51 -0700

    add logging

--- app/jobs/prune_email_messages_job.rb
+++ app/jobs/prune_email_messages_job.rb
@@ -3,9 +3,16 @@ class PruneEmailMessagesJob < ActiveJob::Base
 
   # we only keep email messages for 1.5 years (but give them an extra month to be nice)
   def perform
+    pruner_log = Logger.new("#{Rails.root}/log/prune_email_messages.log")
+    pruner_log.info "RUNNING"
+
     EmailMessage.where('created_at < ?', (1.year.ago - 7.months)).find_each(batch_size: 100) do |email_message|
+      pruner_log.info "  EmailMessage: #{email_message.id}, #{email_message.subject}, user: #{email_message.user_id}, sent: #{email_message.sent_at} - destroyed"
       email_message.destroy
     end
+
+    pruner_log.info "COMPLETED"
+    pruner_log.close
   end
 
 end

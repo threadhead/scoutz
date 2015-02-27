6518c4c25e19de2650854ee3b8b0375f73fd5575
Karl Smith <karl@Karls-iMac.local>
2015-02-26 21:02:47 -0700

    fix log filename

--- app/jobs/prune_public_activity_job.rb
+++ app/jobs/prune_public_activity_job.rb
@@ -3,11 +3,11 @@ class PrunePublicActivityJob < ActiveJob::Base
 
   def perform(days_to_prune=60)
   # def perform(days_to_prune: 60)
-    pruner_log = Logger.new("#{Rails.root}/log/prune_email_messages.log")
+    pruner_log = Logger.new("#{Rails.root}/log/prune_public_activity.log")
     pruner_log.info "RUNNING"
 
     activities = PublicActivity::Activity.where("updated_at < ?", days_to_prune.days.ago)
-    pruner_log.info "  Activities: pruning +#{days_to_prune} days, count: #{activities.count}"
+    pruner_log.info "  PublicActivities: pruning +#{days_to_prune} days, count: #{activities.count}"
     activities.destroy_all
 
     pruner_log.info "COMPLETED"

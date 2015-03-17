e8b6481e99c5a6596379611bb293bdd546e22a9e
Karl Smith <karl@Karls-iMac.local>
2015-03-17 16:42:19 -0700

    some styling tweaks, fix ajax return call, and event print view

--- app/helpers/event_signups_helper.rb
+++ app/helpers/event_signups_helper.rb
@@ -2,4 +2,12 @@ module EventSignupsHelper
   def signup_submit(event_signup)
     "#{event_signup.new_record? ? 'Sign up' : 'Update'} #{event_signup.user.first_name}"
   end
+
+  def signup_well_class(event_signup)
+    if event_signup.persisted?
+      'well-signed-up'
+    else
+      ''
+    end
+  end
 end

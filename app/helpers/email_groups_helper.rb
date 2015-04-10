134f4ed54dbf1361cad50f8c81d2715d134fc1b2
Karl Smith <karl@Karls-iMac.local>
2015-04-09 18:15:49 -0700

    didnt work, order_by causes pg error, back to original with a little cleaning

--- app/helpers/email_groups_helper.rb
+++ app/helpers/email_groups_helper.rb
@@ -2,7 +2,8 @@ module EmailGroupsHelper
   def cache_key_for_email_groups(email_groups)
     # count = email_groups.size
     # max_updated_at = email_groups.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    count_max = email_groups.pluck("COUNT(*)", "MAX(updated_at)")[0].map(&:to_i)
+    # count_max = email_groups.pluck("COUNT(*)", "MAX(updated_at)")[0].map(&:to_i)
+    [email_groups.size, email_groups.maximum(:updated_at)].map(&:to_i)
     "email_groups/index-#{count_max.join('-')}"
   end
 end

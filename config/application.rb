de1505ea3c8dfa3b796d2c445c51e2ea9133db9c
Karl Smith <karl@Karls-iMac.local>
2015-04-10 12:14:55 -0700

    have dotenv load for all environments

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv.load('.env')
+
 module Scoutz
   class Application < Rails::Application
     # config.middleware.use Rack::Deflater
